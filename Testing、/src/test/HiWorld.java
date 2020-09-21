372a84680eb856e07b02984a7155e4f50f39103e
Pangbo <pangbo2016@outlook.com>
2020-09-21 15:22:40 +0800

    test2

--- Testing、/src/test/HiWorld.java
+++ Testing、/src/test/HiWorld.java
@@ -8,5 +8,6 @@ public class HiWorld {
     public static void main(String[] args) {
         System.out.println("Hi World");
         System.out.println("Testing2");
+        System.out.println("Testing3");
     }
 }
