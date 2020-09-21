d8020c45b62bcee68c1edf52042c3102fc677c96
Pangbo <pangbo2016@outlook.com>
2020-09-21 14:26:07 +0800

    test

---
+++ Testing、/src/test/HiWorld.java
@@ -0,0 +1,11 @@
+package test;
+
+/**
+ * @author shkstart
+ * @create 2020-09-21 14:24
+ */
+public class HiWorld {
+    public static void main(String[] args) {
+        System.out.println("Hi World");
+    }
+}
